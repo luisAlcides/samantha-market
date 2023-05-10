e859c6d2b66acbadeb155a69c462c5cb2cb8ba49
luisAlcides <luisalcidesblandon@gmail.com>
2023-05-09 23:00:57 -0600

    resolve problem with room

--- market/forms.py
+++ market/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, User
+from .models import  User
 
 
 class MyUserCreationForm(UserCreationForm):
